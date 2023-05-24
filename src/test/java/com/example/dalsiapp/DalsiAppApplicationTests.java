9438ad7b3fe987d80646e4634235341967ed5366
Tomas Duchek <neb7pxuk@duchic.anonaddy.com>
2023-05-24 18:57:54 +0200

    Initial commit

---
+++ src/test/java/com/example/dalsiapp/DalsiAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.dalsiapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DalsiAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
